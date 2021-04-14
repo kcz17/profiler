39f7f7a9a071453882f97808a7a626e6110cfc6a
Kelvin Zhang <me@iamkelv.in>
2021-04-14 16:30:57 +0100

    Use query API in profiler

--- profiler.go
+++ profiler.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/kcz17/profiler/priority"
-	"time"
 )
 
 // Profiler only has one action: to process user history in the implementation's
@@ -16,29 +14,23 @@ type Profiler interface {
 }
 
 type InfluxDBProfiler struct {
-	client      influxdb2.Client
-	asyncWriter api.WriteAPI
+	client influxdb2.Client
+	query  api.QueryAPI
+	bucket string
 }
 
 func NewInfluxDBProfiler(addr, authToken, org, bucket string) *InfluxDBProfiler {
 	options := influxdb2.DefaultOptions()
 	client := influxdb2.NewClientWithOptions(addr, authToken, options)
-	writeAPI := client.WriteAPI(org, bucket)
-
-	// Create a goroutine for reading and logging async write errors.
-	errorsCh := writeAPI.Errors()
-	go func() {
-		for err := range errorsCh {
-			fmt.Printf("[%s] influxdb2 async write error: %v\n", time.Now().Format(time.StampMilli), err)
-		}
-	}()
+	queryAPI := client.QueryAPI(org)
 
 	return &InfluxDBProfiler{
-		client:      client,
-		asyncWriter: writeAPI,
+		client: client,
+		query:  queryAPI,
+		bucket: bucket,
 	}
 }
 
 func (p *InfluxDBProfiler) Process() map[string]priority.Priority {
-	panic("to be implemented")
+	panic("To be implemented")
 }
